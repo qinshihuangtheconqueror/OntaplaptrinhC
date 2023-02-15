3b8ef90a308535409059d483487008a020cf9445
Lê Hoàng Huy <113824447+qinshihuangtheconqueror@users.noreply.github.com>
2023-02-15 20:42:41 +0700

    Update phanso1.cpp

--- phanso1.cpp
+++ phanso1.cpp
@@ -11,24 +11,24 @@ typedef struct ps{
 int gcd(int a,int b){
 	if(b==0) return a;
 	else return gcd(b,a%b);
-}
+} /* uoc chung lon nhat /*
 int lcm(int a,int b){
 	return a*b/gcd(a,b);
-}
+} /* boi chung nho nhat/*
 ps toigian(ps a){
 	ps res;
 	int uc=gcd(abs(a.tu),abs(a.mau));
 	res.tu= a.tu/uc;
 	res.mau= a.mau/uc;
 	return res;
-}
+} /* tim phan so toi gian */
 ps tong(ps a, ps b){
 	ps res;
 	int mc= lcm(a.mau, b.mau);
 	res.mau=mc;
 	res.tu=mc/ a.mau*a.tu + mc / b.mau* b.tu;
 	return toigian(res);
-}
+} /*tong, hieu 2 phan so o dang toi gian */
 ps  hieu(ps a, ps b){
 	ps res;
 	int mc= lcm(a.mau, b.mau);
